331ac00796f06c790b53b3cd2abe84ff65c40bf1
rishavraj70161@gmail.com <rishavraj7016@gmail.com>
2025-08-12 14:12:53 +0530

    created signup with zod

--- src/db.ts
+++ src/db.ts
@@ -1,1 +1,12 @@
- 
+import { model,Schema } from "mongoose";
+
+console.log("Mongo is connected")
+
+
+const UserSchema = new Schema({
+    username: {type:String, unique: true},
+    password: String,
+    email: {type: String, unique: true},
+})
+
+export const UserModel =  model("User",UserSchema)
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,87 @@
 import express from 'express'
-import mongoose  from 'mongoose'
-const app = express()
-
+import mongoose from 'mongoose'
+import { UserModel } from './db'
+import * as zod from "zod";
 import jwt from 'jsonwebtoken'
+import * as bcrypt from 'bcrypt';
 
-app.post("/api/v1/signup", (req,res) => {
 
-})
+const app = express()
+app.use(express.json())
 
-app.post("/api/v1/signin", (req,res) => {
+app.post("/api/v1/signup", async (req, res) => {
+
+    const requireBody = zod.object({
+        username: zod.string().min(6).max(15),
+        password: zod.string().min(7).max(20),
+        email: zod.string().min(7).email(),
+
+    })
+
+    const parseDatawithSuccess = requireBody.safeParse(req.body)
+
+    if (!parseDatawithSuccess.success) {
+        return res.status(403).json({
+            message: "Incorrect data format",
+            error: parseDatawithSuccess.error,
+        })
+    }
+
+    try {
+        const {
+            username,
+            password,
+            email,
+        } = req.body
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        await UserModel.create({
+            username: username,
+            password: String,
+            email: String,
+        })
+
+        return res.status(200).json({
+            message:"you have Signed Up Successfully"
+        })
+
+
+    } catch (error) {
+        return res.status(200).json({
+            message: "Error while Signing up!"
+        })
+    }
+
     
 })
 
-app.post("/api/v1/content", (req,res) => {
-    
+app.post("/api/v1/signin", (req, res) => {
+
+    const username = req.body.username
+    const password = req.body.password
+    const email = req.body.email
+
+
+
 })
 
-app.get("/api/v1/signup", (req,res) => {
-    
+app.post("/api/v1/content", (req, res) => {
+
 })
 
-app.delete("/api/v1/signup", (req,res) => {
-    
+app.get("/api/v1/signup", (req, res) => {
+
 })
 
-app.post("/api/v1/brain/share", (req,res) => {
-    
+app.delete("/api/v1/signup", (req, res) => {
+
 })
 
-app.get("/api/v1/brain/:shareLink", (req,res) => {
-    
+app.post("/api/v1/brain/share", (req, res) => {
+
+})
+
+app.get("/api/v1/brain/:shareLink", (req, res) => {
+
 })
